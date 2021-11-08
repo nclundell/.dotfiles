0c0ba2695f926bf7a99dc5bd5d9a0bb8424302be
Nathan Lundell <nclundell@pm.me>
2021-11-07 20:08:11 -0500

    update nvim-tree config

--- .config/nvim/lua/configs/treenav.lua
+++ .config/nvim/lua/configs/treenav.lua
@@ -1,10 +1,25 @@
 return function()
-  vim.g.nvim_tree_auto_close = 1
-  vim.g.nvim_tree_follow = 1
   vim.g.nvim_tree_git_hl = 1
-  vim.g.nvim_tree_ignore = { ".git", "node_modules" }
   vim.g.nvim_tree_indent_markers = 1
-  -- g.nvim_tree_show_icons = {"git" = 1, "folders" = 1, "files" = 1}
-  vim.g.nvim_tree_side = "left"
-  vim.g.nvim_tree_width = 50
+
+  require('nvim-tree').setup{
+    auto_close = true,
+    diagnostics = {
+      enable = true
+    },
+    filters = {
+      custom = {
+        ".git",
+        "node_modules"
+      }
+    },
+    update_focused_file = {
+      enable = true
+    },
+    view = {
+      auto_resize = true,
+      hide_root_folder = true,
+      width = 50,
+    }
+  }
 end
