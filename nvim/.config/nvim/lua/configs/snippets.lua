6f49f7596e84da7986358c239b2068275b78450e
Nathan Lundell <nclundell@pm.me>
2021-11-15 21:38:01 -0500

    fix snippets

--- nvim/.config/nvim/lua/configs/snippets.lua
+++ nvim/.config/nvim/lua/configs/snippets.lua
@@ -1,3 +1,8 @@
 return function()
+  require('luasnip').config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI"
+  }
 
+  require("luasnip/loaders/from_vscode").lazy_load()
 end
