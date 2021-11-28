4e550962fc64f54629d902deb2c58471f58ee072
Nathan Lundell <nclundell@pm.me>
2021-11-28 18:45:48 -0500

    customize completion look

--- nvim/.config/nvim/lua/configs/completion.lua
+++ nvim/.config/nvim/lua/configs/completion.lua
@@ -7,11 +7,40 @@ return function()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
+  local kind_icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  }
+
   cmp.setup({
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
+    formatting = {
+      format = function(entry, item)
+	item.kind = string.format('%s  %s', kind_icons[item.kind], item.kind)
+	return item
+      end
     },
     mapping = {
       ["<C-Space>"] = cmp.mapping.complete(),
@@ -50,6 +79,11 @@ return function()
 	end,
 	{'i', 's'}
       ),
+    },
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
     },
     sources = {
       { name = 'nvim_lsp' },
