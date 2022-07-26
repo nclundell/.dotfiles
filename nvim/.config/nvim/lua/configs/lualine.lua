a4536608d78d0d79ef7bbb595280c7ff74491f44
Nathan Lundell <nclundell@pm.me>
2022-07-25 23:09:59 -0400

    lualine winbar

--- nvim/.config/nvim/lua/configs/lualine.lua
+++ nvim/.config/nvim/lua/configs/lualine.lua
@@ -3,6 +3,22 @@ return function()
     options = {
       globalstatus = true,
       theme = 'nord'
+    },
+    winbar = {
+      lualine_a = {'fileformat'},
+      lualine_b = {},
+      lualine_c = {'filename', 'diagnostics', 'diff'},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    inactive_winbar = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename', 'diagnostics', 'diff'},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
     }
   }
 end
