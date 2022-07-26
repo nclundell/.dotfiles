1d198ab035ac5407447eb8f3e3a1c8533a778c81
Nathan Lundell <nathan.lundell@andros.co>
2022-07-26 17:48:25 -0400

    add telescope oldfiles keybind

--- nvim/.config/nvim/lua/configs/telescope/keybinds.lua
+++ nvim/.config/nvim/lua/configs/telescope/keybinds.lua
@@ -30,7 +30,8 @@ require('which-key').register({
     f = { function() require('telescope.builtin').find_files() end, "File" },
     g = { function() require('telescope.builtin').git_files() end, "Versioned (Git) File" },
     h = { function() require('telescope.builtin').help_tags() end, "Help"},
-    m = { function() require('telescope.builtin').man_pages() end, "Man Pages"}
+    m = { function() require('telescope.builtin').man_pages() end, "Man Pages"},
+    r = { function() require('telescope.builtin').oldfiles({ prompt_title = "Recent Files" }) end, "Recent Files" }
   },
   s = {
     name = 'Select',
