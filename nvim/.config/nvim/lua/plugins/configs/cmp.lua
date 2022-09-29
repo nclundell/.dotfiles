b7c0967f7f3537272d79a496779b303a8ef6a222
Nathan Lundell <nathan.lundell@andros.co>
2022-09-29 15:28:14 -0400

    better completion functionality

--- nvim/.config/nvim/lua/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -9,9 +9,6 @@ local has_words_before = function()
 end
 
 cmp.setup({
-  completion = {
-    autocomplete = false
-  },
   formatting = {
     format = require('lspkind').cmp_format(),
   },
@@ -22,7 +19,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm { select = true },
+    ['<CR>'] = cmp.mapping.confirm { select = false },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
