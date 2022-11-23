b03dbb1fc044adc428700cbba9175bb991def200
Nathan Lundell <nathan.lundell@andros.co>
2022-11-23 11:03:32 -0500

    minor telescope reorganization

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -1,4 +1,6 @@
-require('telescope').setup{
+local telescope = require('telescope')
+
+telescope.setup{
 	defaults = {
 		file_ignore_patterns = { 'node_modules' },
 		layout_config = {
@@ -16,8 +18,8 @@ require('telescope').setup{
 }
 
 -- Load Extensions
-require('telescope').load_extension('env')
-require('telescope').load_extension('file_browser')
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('toggletasks')
-require('telescope').load_extension('ui-select')
+telescope.load_extension('env')
+telescope.load_extension('file_browser')
+telescope.load_extension('fzf')
+telescope.load_extension('toggletasks')
+telescope.load_extension('ui-select')
