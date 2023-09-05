c6cb8a7ee907099df6d422d22eb5e42fbfc32b8e
Nathan Lundell <nclundell@pm.me>
2023-09-04 21:49:05 -0400

    update statusbar

--- nvim/.config/nvim/after/plugin/configs/status_lines.lua
+++ nvim/.config/nvim/after/plugin/configs/status_lines.lua
@@ -7,32 +7,33 @@ require('lualine').setup{
   options = {
     section_separators = { left = '', right = '' },
     globalstatus = true,
+    path = 1,
     refresh = {
       winbar = 100
     }
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'diff', 'diagnostics'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
     lualine_x = {},
     lualine_y = {'encoding', 'fileformat', 'filetype'},
     lualine_z = {'location'}
-  },
-  winbar = {
-    lualine_a = {'filename'},
-    lualine_b = {'diff'},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  inactive_winbar = {
-    lualine_a = {},
-    lualine_b = {'filename'},
-    lualine_c = {'diff'},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
   }
+  -- winbar = {
+  --   lualine_a = {'filename'},
+  --   lualine_b = {'diff'},
+  --   lualine_c = {},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
+  -- inactive_winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {'filename'},
+  --   lualine_c = {'diff'},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- }
 }
