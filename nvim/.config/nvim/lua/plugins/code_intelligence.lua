51b8cfc34602b5d34acb3c846f41f9c8e6df93ea
Nathan Lundell <nclundell@pm.me>
2024-02-19 05:54:30 +0000

    return to cmp

--- nvim/.config/nvim/lua/plugins/code_intelligence.lua
+++ nvim/.config/nvim/lua/plugins/code_intelligence.lua
@@ -1,22 +1,70 @@
 return {
   {
-    'echasnovski/mini.completion',
-    opts = {},
-    version = '*',
-    init = function()
-      vim.keymap.set(
-        'i',
-        '<Tab>',
-        [[pumvisible() ? "\<C-n>" : "\<Tab>"]],
-        { expr = true }
-      )
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      -- Snippets
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
 
-      vim.keymap.set(
-        'i',
-        '<S-Tab>',
-        [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]],
-        { expr = true }
-      )
+      -- Sources
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-omni',
+      'hrsh7th/cmp-path',
+      'kristijanhusak/vim-dadbod-completion',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- UI
+      'onsails/lspkind-nvim'
+    },
+    config = function()
+      local lspkind = require('lspkind')
+      local luasnip = require('luasnip')
+      local cmp = require('cmp')
+
+      cmp.setup({
+        formatting = {
+          format = lspkind.cmp_format()
+        },
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        sources = {
+          { name = 'dadbod' },
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lua' },
+          { name = 'luasnip' },
+          { name = 'omni' },
+          { name = 'path' },
+          { name = 'buffer' }
+        }
+      })
     end
   },
   -- {
@@ -56,7 +104,15 @@ return {
 
       lsp.bashls.setup {}
       lsp.gopls.setup {}
-      lsp.lua_ls.setup {}
+      lsp.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
+      }
       lsp.pyright.setup {}
       lsp.rust_analyzer.setup {}
       lsp.solargraph.setup {}
