f94be267b3398f2f4ea5dbe694f25bd935ce16c0
Nathan Lundell <nclundell@pm.me>
2024-02-20 01:42:52 +0000

    add transparent colorscheme

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,12 +1,14 @@
 return {
   'catppuccin/nvim',
   'olimorris/onedarkpro.nvim',
+  'xiyaowong/transparent.nvim',
   {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     opts = {
       style = 'moon',
+      transparent = vim.g.transparent_enabled
     },
   }
 }
