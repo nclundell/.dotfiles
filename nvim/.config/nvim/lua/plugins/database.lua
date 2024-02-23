bd63019b1057ee81fdcab268b9b0d73a5806fd5f
Nathan Lundell <nlundell@churchinitiative.org>
2024-02-22 20:55:32 -0500

    update database settings

--- nvim/.config/nvim/lua/plugins/database.lua
+++ nvim/.config/nvim/lua/plugins/database.lua
@@ -1,10 +1,8 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    {
-      'tpope/vim-dadbod',
-      lazy = true
-    },
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
   },
   cmd = {
     'DBUI',
@@ -14,5 +12,5 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
-  end,
+  end
 }
