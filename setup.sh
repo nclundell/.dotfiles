112314eddc6361f15b75ea59d916d6c7d0375499
Nathan Lundell <nclundell@pm.me>
2024-08-21 23:10:47 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -2,18 +2,12 @@
 
 mkdir -p $HOME/.local/bin
 
-if [[ -f $HOME/.bashrc ]]; then
-  if [[ ! -L $HOME/.bashrc ]]; then
-    mv $HOME/.bashrc $HOME/.bashrc.bak
-  fi
-fi
-
 stow .
 
 source $HOME/.bashrc
 
 if [[ ! -x "$(command -v mise)" ]]; then
-  uget mise
+  uget mise tmp
 fi
 
 source $HOME/.local/installers/build-deps
