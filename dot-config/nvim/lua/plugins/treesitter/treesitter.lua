235c50f70e80cdd539e9b5d71d8763487e88f68f
Nathan Lundell <nclundell@pm.me>
2024-12-15 21:27:24 -0500

    treesitter incremental selection

--- dot-config/nvim/lua/plugins/treesitter/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -1,10 +1,24 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = {
+    'BufReadPre',
+    'BufNewFile'
+  },
   main = 'nvim-treesitter.configs',
   opts = {
     auto_install = true,
     highlight = {
       enable = true
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-Space>',
+        node_incremental = '<C-Space>',
+        scope_incremental = false,
+        node_decremental = '<BS>',
+      }
     },
     indentation = {
       enable = true
