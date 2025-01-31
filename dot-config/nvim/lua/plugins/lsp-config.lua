62ceb89c3fe77e12f492b94185b2d8db5cb49eda
Nathan Lundell <nclundell@pm.me>
2025-01-31 18:25:57 -0500

    add vscode-extracted lsps

--- dot-config/nvim/lua/plugins/lsp-config.lua
+++ dot-config/nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,11 @@ return {
 
     lsp.basedpyright.setup { capabilities = capabilities }
     lsp.bashls.setup { capabilities = capabilities }
+    lsp.cssls.setup { capabilities = capabilities }
     lsp.ember.setup { capabilities = capabilities }
+    lsp.eslint.setup { capabilities = capabilities }
+    lsp.html.setup { capabilities = capabilities }
+    lsp.jsonls.setup { capabilities = capabilities }
     lsp.lua_ls.setup { capabilities = capabilities }
     lsp.ruby_lsp.setup { capabilities = capabilities }
     -- lsp.solargraph.setup { capabilities = capabilities }
