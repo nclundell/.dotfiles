4a22e518d8d42355b0d74311a48c096b3d563b82
Nathan Lundell <nclundell@pm.me>
2025-02-11 21:51:41 -0500

    simplify setup script

--- setup.sh
+++ setup.sh
@@ -17,37 +17,3 @@ if [[ ! -x "$(command -v mise)" ]]; then
 fi
 
 source $HOME/.bashrc
-
-if [[ ! -x "$(command -v lsb_release)" ]]; then
-  if [[ -x "$(command -v pacman)" ]]; then
-    sudo pacman -S --noconfirm --needed lsb-release
-  fi
-fi
-
-if [[ $(lsb_release -is) == *Arch* ]]; then
-  echo "Adding Chaotic AUR..."
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/chaotic-aur
-
-  echo "Updating mirrors..."
-  source $HOME/.dotfiles/dot-local/scripts/arch-mirror-update
-
-  source $HOME/.dotfiles/dot-local/installers/build-deps
-
-  echo "Running Arch installers..."
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/bluetooth
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/codecs
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/drivers-gpu
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/fonts
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/icons
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/tools-cli
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/tools-gui
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/yay
-  source $HOME/.dotfiles/dot-local/installers/distros/arch/snapper
-elif [[ $(lsb_release -is) == *Fedora* ]]; then
-  echo "Running Fedora installers..."
-  source $HOME/.dotfiles/dot-local/installers/distros/fedora/codecs
-fi
-
-source $HOME/.bashrc
-
-mise i
