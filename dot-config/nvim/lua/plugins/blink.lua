4d7385c0353e3305e00d7680526f34acfc3260b5
Nathan Lundell <nclundell@pm.me>
2025-04-09 18:58:21 -0400

    no longer need lsp capabilities per nvim 0.11 update

--- dot-config/nvim/lua/plugins/blink.lua
+++ dot-config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
   'saghen/blink.cmp',
   dependencies = 'rafamadriz/friendly-snippets',
-  version = '*',
+  version = '1.*',
   opts = {
     keymap = {
       ['<TAB>'] = {
--- dot-config/nvim/lua/plugins/lsp-config.lua
+++ dot-config/nvim/lua/plugins/lsp-config.lua
@@ -1,22 +1,21 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
     local lsp = require('lspconfig')
 
-    lsp.basedpyright.setup { capabilities = capabilities }
-    lsp.bashls.setup { capabilities = capabilities }
-    lsp.cssls.setup { capabilities = capabilities }
-    lsp.ember.setup { capabilities = capabilities }
-    lsp.eslint.setup { capabilities = capabilities }
-    lsp.html.setup { capabilities = capabilities }
-    lsp.jsonls.setup { capabilities = capabilities }
-    lsp.lua_ls.setup { capabilities = capabilities }
-    lsp.ruby_lsp.setup { capabilities = capabilities }
-    -- lsp.solargraph.setup { capabilities = capabilities }
-    lsp.stimulus_ls.setup { capabilities = capabilities }
-    lsp.tailwindcss.setup { capabilities = capabilities }
-    lsp.ts_ls.setup { capabilities = capabilities }
-    lsp.yamlls.setup { capabilities = capabilities }
+    lsp.basedpyright.setup {}
+    lsp.bashls.setup {}
+    lsp.cssls.setup {}
+    lsp.ember.setup {}
+    lsp.eslint.setup {}
+    lsp.html.setup {}
+    lsp.jsonls.setup {}
+    lsp.lua_ls.setup {}
+    lsp.ruby_lsp.setup {}
+    -- lsp.solargraph.setup {}
+    lsp.stimulus_ls.setup {}
+    lsp.tailwindcss.setup {}
+    lsp.ts_ls.setup {}
+    lsp.yamlls.setup {}
   end
 }
