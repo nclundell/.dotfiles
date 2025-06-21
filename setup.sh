74b1031313d4864e6b79d81d5ce6fc08b8b0b98d
Nathan Lundell <nclundell@pm.me>
2025-06-20 22:14:38 -0400

    strip homebrew from setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
-mkdir -p $HOME/.local/bin
-
 if [[ -f $HOME/.bashrc ]]; then
   if [[ -L $HOME/.bashrc ]]; then
     rm $HOME/.bashrc
@@ -12,16 +10,8 @@ fi
 
 stow .
 
-# Always install Homebrew on Darwin
-if [[ $(uname) == "Darwin" ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Run brew bundle if Brew installed, otherwise install Mise
-if [[ -x "$(command -v brew)" ]]; then
-  brew bundle --file $HOME/.dotfiles/Brewfile
-else
-  curl https://mise.run | sh
-fi
+# Install Mise
+mkdir -p $HOME/.local/bin
+curl https://mise.run | sh
 
 source $HOME/.bashrc
