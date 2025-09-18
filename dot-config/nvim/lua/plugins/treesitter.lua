5e70c66870b4714d1741334621a56de85b82d99d
Nathan Lundell <nclundell@pm.me>
2025-09-17 22:45:50 -0400

    upgrade to new treesitter

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,21 @@
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    local ft = vim.bo.filetype
+    if require('nvim-treesitter.parsers')[ft] then
+      require('nvim-treesitter').install(ft, {}, function()
+        vim.treesitter.start()
+      end)
+    end
+  end
+})
+
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
   dependencies = {
-    'RRethy/nvim-treesitter-endwise',
+    'RRethy/nvim-treesitter-endwise'
   },
-  event = {
-    'BufReadPre',
-    'BufNewFile'
-  },
-  main = 'nvim-treesitter.configs',
-  opts = {
-    auto_install = true,
-    highlight = {
-      enable = true
-    }
-  }
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate'
 }
