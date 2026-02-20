0c341a9b7ab17599186a69034259a5006fad2ff6
Nathan Lundell <nclundell@pm.me>
2026-02-20 18:37:29 -0500

    update diagnostics config

--- dot-config/nvim/lua/core/diagnostics.lua
+++ dot-config/nvim/lua/core/diagnostics.lua
@@ -1,6 +1,10 @@
 -- Diagnostics --
 vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true
-  }
+	virtual_lines = {
+		current_line = true,
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
